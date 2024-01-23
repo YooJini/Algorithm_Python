6279e9d48c33d6de5d18947070e722d5e79a2d27
YooJini <63229144+YooJini@users.noreply.github.com>
2024-01-23 18:45:04 +0900

    [level 1] Title: 모의고사, Time: 2.64 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.59 ms
+메모리: 10.3 MB, 시간: 2.64 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 18:37:18
+2024년 1월 2일 18:45:5
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,22 +3,18 @@ def solution(answers):
     one = [1,2,3,4,5]
     two = [2,1,2,3,2,4,2,5]
     three = [3,3,1,1,2,2,4,4,5,5]
-    score = {'1':0,'2':0,'3':0}
+    score = [0,0,0]
     answer = []
-    for i in range(len(answers)):
-        if one[i%5] == answers[i]:
-            score['1'] += 1
-        if two[i%8] == answers[i]:
-            score['2'] += 1
-        if three[i%10] == answers[i]:
-            score['3'] += 1
-  
-    score = sorted(score.items(), key=lambda x:x[1], reverse = True)
-    max = 0
-    for i in score:
-        if max <= i[1]:
-            answer.append(int(i[0]))
-            max = i[1]
-        else: break
+    for idx, ans in enumerate(answers):
+        if one[idx%5] == answers[idx]:
+            score[0] += 1
+        if two[idx%8] == answers[idx]:
+            score[1] += 1
+        if three[idx%10] == answers[idx]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if max(score) == s:
+            answer.append(idx+1)
         
     return(answer)
