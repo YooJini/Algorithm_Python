fb4cc6aecb886085cee79846c28198810e766339
YooJini <63229144+YooJini@users.noreply.github.com>
2024-01-23 17:43:51 +0900

    [level 1] Title: 최소직사각형, Time: 3.60 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/README.md
+++ 프로그래머스/1/86491. 최소직사각형/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최소직사각형 - 86491 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86491) 
 
 ### 성능 요약
 
-메모리: 11.3 MB, 시간: 1.72 ms
+메모리: 11.3 MB, 시간: 3.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 16:59:27
+2024년 1월 2일 17:43:53
 
 ### 문제 설명
 
--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -1,15 +1,12 @@
 def solution(sizes):
 
-    max_1 = 0
-    for i in sizes:
-        if i[0] < i[1]:
-            i[0],i[1] = i[1],i[0]
-        if max_1 < i[0]:
-            max_1 = i[0]
+    w = 0
+    h = 0
     
-    max_2 = 0
-    for i in sizes:
-        if max_2 < i[1]:
-            max_2 = i[1]
-
-    return max_1 * max_2
+    for a, b in sizes:
+        if a < b:
+            b,a = a,b
+        w = max(w, a)
+        h = max(h, b)
+    
+    return w * h
